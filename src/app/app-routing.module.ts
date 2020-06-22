d40a7492e640a4d99d3cac034dfd79180ae03064
Derek Nie <dnie63@berkeley.edu>
2020-06-22 16:39:32 -0400

    added basic routing functionality between the featured and personalize components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { FeaturedComponent } from './products/featured/featured.component';
+import { PersonalizeComponent } from './products/personalize/personalize.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'featured'},
+  { path: 'featured', component: FeaturedComponent },
+  { path: 'personalize', component: PersonalizeComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
